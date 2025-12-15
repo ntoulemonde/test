551298b845e7cdf75c829128d549588929a21118
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2025-12-15 14:59:14 +0000

    with ex 2

--- SQL/sql.py
+++ SQL/sql.py
@@ -1,6 +1,14 @@
 import duckdb
 
+
 def my_connect(path_file, df_name, requete):
+    '''
+    Function to create a view from a path and have a SQL requete on it
+    
+    Returns : 
+        None
+        But it prints the result of the SQL requete
+    '''
     # Création de la connexion
     con = duckdb.connect(database=':memory:')  # Utilise une base de données en mémoire
     # Création d'une vue vers le fichier Parquet
@@ -28,11 +36,10 @@ my_connect(
     LIMIT 5;
 """
 )
-# Définition de la requête SQL
 
 
+# Exercice 2
 """
--- Exercice 2
 -- On souhaite créer la table rp2020_guadeloupe des individus résidant en Guadeloupe
 -- (DEPT = '971' ) comprenant les variables suivantes :
 -- REG, la région codée sur deux caractères à partir de la variable REGION
@@ -43,48 +50,41 @@ my_connect(
 --  IPONDI nombre d'individus ayant les mêmes caractéristiques (pondération)
 -- Classer la table dans l'ordre des années de naissance ANAIS
 --  Visualliser les 10 premières lignes
+"""
+my_connect(
+    path_file='https://static.data.gouv.fr/resources/recensement-de-la-population-fichiers-detail-individus-localises-au-canton-ou-ville-2020-1/20231023-122841/fd-indcvi-2020.parquet', 
+    df_name='rp2020',
+    requete="""
+    SELECT
+        LPAD(REGION, 2, '0') AS REGION,
+        CASE
+            WHEN SEXE = 1 THEN 'Hommes'
+            WHEN SEXE = 2 THEN 'Femmes'
+            ELSE 'inconnu'
+        END AS SEXE,
+        AGED,
+        ANAI AS ANAIS,
+        2020-AGED AS ANAIS1,
+        IPONDI,
+    FROM
+        rp2020
+    WHERE
+        DEPT = '971'
+    ORDER BY
+        ANAIS
+    LIMIT 10;
+    """
+)
 
-SELECT
-	LPAD(REGION, 2, '0') AS REGION,
-	CASE
-		WHEN SEXE = 1 THEN 'Hommes'
-		WHEN SEXE = 2 THEN 'Femmes'
-		ELSE 'inconnu'
-	END AS SEXE,
-	AGED,
-	ANAI AS ANAIS,
-	2020-AGED AS ANAIS1,
-	IPONDI,
-FROM
-	rp2020
-WHERE
-	DEPT = '971'
-ORDER BY
-	ANAIS
-LIMIT 10;
 
-
+"""
 -- Exercice 3
-
-CREATE OR REPLACE
-VIEW rp2020 AS
-FROM
-'https://static.data.gouv.fr/resources/recensement-de-la-population-fichiers-detail-individus-localises-au-canton-ou-ville-2020-1/20231023-122841/fd-indcvi-2020.parquet';
 -- On souhaite afficher la population des cinq départements du Grand Est (REGION 44) les plus peuplés, classés du plus grand au plus petit. (IPONDI = variable de pondération)
 -- On pourra arrondir la population avec la fonction ROUND
+"""
 
-SELECT
-	DEPT,
-	ROUND(SUM(IPONDI))::INT AS POP
-FROM
-	rp2020
-WHERE
-	REGION = '44'
-GROUP BY
-	DEPT
-ORDER BY
-	POP DESC
-LIMIT 5;
+
+"""
 -- Exercice 4
 -- Reprendre le script de l'exercice 2 (Population par département du Grand Est)
 -- Ajouter le nom des départements à l'aide d'une jointure avec la table cog_dep
